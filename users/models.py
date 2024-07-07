b62d941145c9758ce4eff97f86f85f2c96a007e2
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2024-07-07 22:03:07 +0300

    Modify user, product and service models

--- users/models.py
+++ users/models.py
@@ -1,11 +1,15 @@
 from uuid import uuid4
 
+from django.contrib.auth import get_user
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
 
-from users.const import (PAYER_ACCOUNT_NUMBER_MAX_LEN, FIRST_NAME_MAX_LEN,
-                         LAST_NAME_MAX_LEN, PATRONYMIC_MAX_LEN)
+from users.base_model import BaseModel
+from users import const 
 from decimal import Decimal
+
+
+User = get_user()
 
 
 class UserProfileManager(BaseUserManager):
@@ -30,37 +34,56 @@ class UserProfileManager(BaseUserManager):
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     email = models.EmailField('Адрес электронной почты', unique=True)
-    first_name = models.CharField('Имя', max_length=FIRST_NAME_MAX_LEN)
-    last_name = models.CharField('Фамилия', max_length=LAST_NAME_MAX_LEN)
+    first_name = models.CharField('Имя', max_length=const.FIRST_NAME_MAX_LEN)
+    last_name = models.CharField('Фамилия', max_length=const.LAST_NAME_MAX_LEN)
     patronymic = models.CharField(
         'Отчество',
-        max_length=PATRONYMIC_MAX_LEN,
+        max_length=const.PATRONYMIC_MAX_LEN,
         blank=True,
         null=True
     )
-    payer_account_number = models.CharField(
+    unp = models.CharField(
         'Учетный номер плательщика',
-        max_length=PAYER_ACCOUNT_NUMBER_MAX_LEN,
+        max_length=const.PAYER_ACCOUNT_NUMBER_MAX_LEN,
         blank=True,
         null=True
     )
-    secret_word = models.CharField(blank=True)
+    registration_address = models.CharField(
+        'Адрес регистрации',
+        max_length=const.ADDRESS_MAX_LEN,
+        blank=True,
+        null=True
+    )
+    residential_address = models.CharField(
+        'Адрес проживания',
+        max_length=const.ADDRESS_MAX_LEN,
+        blank=True,
+        null=True
+    )
+    avatar = models.ImageField(upload_to='users_avatars', null=True, blank=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    secret_word = models.CharField(blank=True, null=True)
     created_at = models.DateField(auto_now_add=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     objects = UserProfileManager()
 
+    @property
+    def is_npd_payer(self):
+        return bool(self.unp)
+
     class Meta:
-        ordering = ('email',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
     def __str__(self):
         return f'User(email={self.email}, is_active={self.is_active})'
 
 
-class SignupSession(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+class SignupSession(BaseModel):
     email = models.EmailField()
     confirm_code = models.PositiveIntegerField()
     expiration_time = models.DateTimeField()
@@ -69,68 +92,76 @@ class SignupSession(models.Model):
         return f'SighupSession(email={self.email}, confirm_code={self.confirm_code})'
 
 
-class User(models.Model):
-    email = models.ForeignKey(UserProfile, on_delete=models.CASCADE, verbose_name="Email")
-    first_name = models.ForeignKey(UserProfile, on_delete=models.CASCADE, verbose_name="Имя")
-    last_name = models.ForeignKey(UserProfile, on_delete=models.CASCADE, verbose_name="Фамилия")
-    patronymic = models.ForeignKey(UserProfile, on_delete=models.CASCADE, verbose_name="Отчество")
-    personal_number = models.CharField(max_length=14, unique=True)
-    registration_address = models.TextField(max_length=1000, verbose_name="Адрес регистрации", default="DEFAULT VALUE")
-    residential_address = models.TextField(max_length=1000, verbose_name="Адрес проживания", default="DEFAULT VALUE")
-    photo = models.ImageField(upload_to="users_photos/", null=True, blank=True)
-    payer_account_number = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.last_name} {self.first_name} {self.patronymic}"
+class ProductCategory(BaseModel):
+    name = models.CharField(
+        'Наименование категории товара',
+        max_length=const.CATEGOTY_NAME_MAX_LEN
+    )
 
     class Meta:
-        verbose_name = "Пользователь"
-        verbose_name_plural = "Пользователи"
-
-
-class ProductCategory(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Наименование категории товара")
+        verbose_name = 'Категория продукта'
+        verbose_name_plural = 'Категории продуктов'
 
     def __str__(self):
-        return self.name
+        return self.name[:30]
 
 
-class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Наименование товара")
-    product_description = models.TextField(max_length=1000, verbose_name="Описание товара")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Стоимость товара")
-    count = models.PositiveIntegerField(verbose_name="Количество товара в наличии")
+class Product(BaseModel):
+    name = models.CharField('Наименование товара', max_length=const.PRODUCT_NAME_MAX_LEN)
+    description = models.CharField('Описание товара', max_length=const.PRODUCT_DESCRIP_MAX_LEN)
+    price = models.DecimalField('Стоимость товара', max_digits=10, decimal_places=2)
+    count = models.PositiveIntegerField(
+        'Количество товара в наличии',
+        null=True,
+        blank=True
+    )
+    #TODO not sure: 1 to many or many to many?
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
-    reviews = models.ManyToManyField(Review, related_name="products")
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        default_related_name = 'products'
+
+    def __str__(self):
+        return self.name[:30]
+
+
+class ServiceCategory(BaseModel):
+    name = models.CharField(
+        'Наименование категории услуги',
+        max_length=const.CATEGOTY_NAME_MAX_LEN
+    )
+
+    def __str__(self):
+        return self.name[:30]
+
+
+class Service(BaseModel):
+    name = models.CharField('Наименование услуги', max_length=const.SERVICE_NAME_MAX_LEN)
+    service_description = models.CharField('Описание услуги', max_length=const.SERVICE_DESCRIP_MAX_LEN)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Стоимость услуги')
+    #TODO not sure: 1 to many or many to many?
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
+        default_related_name = 'services'
 
     def __str__(self):
         return self.name
 
 
-class ServiceCategory(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Наименование категории сервиса")
-
-    def __str__(self):
-        return self.name
-
-
-class Service(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Наименование услуги")
-    service_description = models.TextField(max_length=1000, verbose_name="Описание услуги")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Стоимость услуги")
-    category = models.ForeignKey(ServiceCategory, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
-class Activities(models.Model):
-    name = models.CharField(max_length=255, verbose_name="Вид деятельности")
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    service = models.ForeignKey(Service, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
+#class Activities(models.Model):
+#    name = models.CharField(max_length=255, verbose_name="Вид деятельности")
+#    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+#
+#    def __str__(self):
+#        return self.name
 
 
 class Order(models.Model):
