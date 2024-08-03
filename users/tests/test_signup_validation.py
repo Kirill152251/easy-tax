36231e5a8c8a0609d9ab94e07b64b7ce5f0a2bc9
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2024-08-03 14:35:21 +0300

    Fix bug by modifying signup endpoint

--- users/tests/test_signup_validation.py
+++ users/tests/test_signup_validation.py
@@ -1,5 +1,7 @@
 import pytest
 from django.contrib.auth import get_user_model
+from django.core import mail
+from django.urls import reverse
 from rest_framework import status
 
 
@@ -74,5 +76,32 @@ def test_invalid_secret_word_contain_digits(client, signup_url, signup_body):
 )
 def test_fio_only_dashe(client, signup_url, signup_body, fio):
     signup_body[fio] = '-'
+    response = client.post(signup_url, data=signup_body)
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_same_email_but_uppercase(client, signup_url, signup_body):
+    response = client.post(signup_url, data=signup_body)
+    assert response.status_code == status.HTTP_201_CREATED
+    confirm_code_id = response.data['confirm_code_id']
+    email_massage = mail.outbox[0].body
+    code = ''.join(d for d in email_massage if d.isdigit())
+    response = client.post(reverse('users:confirm_code', args=(code, confirm_code_id)))
+    assert response.status_code == status.HTTP_200_OK
+    new_body = {
+        'email': 'USER@gmail.com',
+        'password': 'sTring32%21',
+        'first_name': 'телс',
+        'last_name': 'афдоафо',
+        'patronymic': 'фаощшфофо',
+        'secret_word': 'слово'
+    }
+    response = client.post(signup_url, data=new_body)
+    assert response.status_code == status.HTTP_202_ACCEPTED
+
+
+def test_signup_without_email(client, signup_url, signup_body):
+    signup_body.pop('email')
     response = client.post(signup_url, data=signup_body)
     assert response.status_code == status.HTTP_400_BAD_REQUEST
